90de7a55521fabb9895a99fafd2780849f0cdeb0
Gopi515 <gopinathghosh514@gmail.com>
2024-08-30 20:06:59 +0530

    complete counterApp code done

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-let value = document.getElementById("counter");
-let plusButton = document.getElementById("increase");
-let minusButton = document.getElementById("decrease");
+let countValue = document.getElementById("counter");
+let incrementButton = document.getElementById("increase");
+let decrementButton = document.getElementById("decrease");
 
-value.textContent = 0;
+// function increment() {
+//     countValue.innerText = parseInt(countValue.innerText) + 1;
+// }
 
-plusButton.addEventListener("click", () => {
-  value.textContent = parseInt(value.textContent) + 1;
-});
+// function decrement() {
+//     countValue.innerText = parseInt(countValue.innerText) - 1;
+// }
 
-minusButton.addEventListener("click", () => {
-  value.textContent = parseInt(value.textContent) - 1;
-});
+function increment(){
+    // get the vlue from UI
+    let value = parseInt(countValue.innerText);
+    //Update the value
+    value = value + 1;
+    //set the value onto UI
+    countValue.innerText = value;
+};
+
+
+function decrement(){
+        // get the vlue from UI
+        let value = parseInt(countValue.innerText);
+        //Update the value
+        value = value - 1;
+        //set the value onto UI
+        countValue.innerText = value;
+};
+
+// let value = 0;
+// incrementButton.addEventListener("click", function () {
+//     value = value + 1;
+//     countValue.innerText = value;
+// });
+
+// decrementButton.addEventListener("click", function () {
+//     value = value - 1;
+//     countValue.innerText = value;
+// });
 
